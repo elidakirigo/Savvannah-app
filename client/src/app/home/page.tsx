827f5465e8224ddeb3448be5b4676c9cbcfc448b
elida <elidakirigo@gmail.com>
2024-05-25 23:17:23 +0300

    test(cypress): add layout tests

---
+++ client/src/app/home/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function page() {
+	fetch('https://jsonplaceholder.typicode.com/todos/1')
+		.then((response) => response.json())
+		.then((json) => console.log(json))
+	return <div>hello</div>
+}
