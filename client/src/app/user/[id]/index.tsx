116bae71e7b4dbd5523cecb3df6f58ffdd52e4a1
elida <elidakirigo@gmail.com>
2024-05-26 18:23:58 +0300

    fix: fix react naming convention on pages

--- client/src/app/user/[id]/index.tsx
+++ client/src/app/user/[id]/index.tsx
@@ -23,7 +23,7 @@ const page = () => {
 				<tbody>
 					<tr className='border-b border-gray-200 dark:border-gray-700'>
 						<th scope='row' className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800'>
-							Apple MacBook Pro 17"
+							Apple MacBook Pro 17&quot;
 						</th>
 						<td className='px-6 py-4'>Silver</td>
 						<td className='px-6 py-4 bg-gray-50 dark:bg-gray-800'>Laptop</td>
