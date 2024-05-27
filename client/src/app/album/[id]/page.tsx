066c7252756e311b2876b2a181cc8a3319c89cc8
elida <elidakirigo@gmail.com>
2024-05-27 15:17:20 +0300

    chore: add photo edit fn

--- client/src/app/album/[id]/page.tsx
+++ client/src/app/album/[id]/page.tsx
@@ -16,26 +16,53 @@ const Page = ({ params: { id } }: Props) => {
   const currentAlbum = AlbumPhotos[Number(id) - 1];
 
   return (
-    <div className="max-w-[] flex flex-wrap gap-5 p-4">
-      {currentAlbum?.map(({ thumbnailUrl, title, id, url }) => (
-        <div
-          className="max-w-[250px] rounded overflow-hidden shadow-lg cursor-pointer"
-          onClick={() => router.push("/photo/" + id)}
-          key={id}
-        >
-          <Image className="w-full" src={thumbnailUrl} alt={title} />
-          <div className="py-4">
-            <div className="font-semibold  mb-2">
-              <div className="px-6 pt-4 pb-2">
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                  {id}
-                </span>
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg p-5 ">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+              image title
+            </th>
+            <th scope="col" className="px-6 py-3">
+              image
+            </th>
+            <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+              image id
+            </th>
+            <th scope="col" className="px-6 py-3">
+              album id
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentAlbum?.map(({ thumbnailUrl, title, id, url, albumId }) => (
+            <tr
+              className="border-b border-gray-200 dark:border-gray-700"
+              onClick={() => router.push("/photo/" + id)}
+              key={id}
+            >
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+              >
                 {title}
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
+              </th>
+              <td className="px-6 py-4 hover:bg-slate-600">
+                <Image
+                  className="w-full"
+                  src={thumbnailUrl}
+                  alt={title}
+                  width={10}
+                  height={10}
+                  unoptimized={true}
+                />
+              </td>
+              <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">{id}</td>
+              <td className="px-6 py-4">{albumId}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,17 +5,30 @@ import Navbar from "../../components/navbar";
 
 function Homepage() {
   const { data: session } = useSession();
+
   return (
     <>
       <div className="flex top-16 p-8 relative justify-start items-center flex-col text-center ">
         <h1 className="text-[25px] md:text-[40px] pb-5 ">
           Welcome to Savannah App
         </h1>
-        <p className="max-w-[500px]">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime esse
-          expedita eveniet explicabo at cumque! Laudantium minus quod aliquid
-          repellendus facilis vel cupiditate saepe itaque illum, vero, voluptas
-          distinctio ea.
+        <p className="max-w-[700px] ">
+          This web application allows users to manage and view user information,
+          albums, and photos through a responsive and polished interface.
+          <span>
+            <strong> Key features include:</strong>
+            <strong>Landing Page: </strong>
+            <strong>Authentication:</strong> Secure login via Google.
+            <strong>User Management:</strong>
+            <strong>User List: </strong>Displays all users and their album
+            counts.
+            <strong>User Detail:</strong> Shows user information and their
+            albums.
+            <strong>Album Management: </strong>Album Detail: Lists details of
+            selected albums and photos.
+            <strong>Photo Management:</strong> Photo Detail: Allows editing of
+            photo titles with backend updates.
+          </span>
         </p>
 
         {!session && (
